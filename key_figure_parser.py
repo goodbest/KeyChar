86368090976138b6abffc6499a31158bcdc909b0
goodbest <lovegoodbest@gmail.com>
2015-03-18 11:12:55 +0800

    use javascript instead of php for nearest birthday display

--- key_figure_parser.py
+++ key_figure_parser.py
@@ -5,6 +5,7 @@ import json
 import csv
 import sys
 import os.path
+import datetime
 
 
 def json2csv(infile, outfile):
@@ -38,7 +39,7 @@ def csv2json(infile, outfile, xe=False):
                                  '<tr><td>CV:</td><td align="right">%s</tr>' \
                                  '</tbody></table></div>' \
                                 %(row['Game'], row['Height'], row['Weight'], row['Blood'], row['B'], row['W'], row['H'], row['Cup'], row['CV']),
-                    'startDate': '2000,%s,%s' %(row['BirthMonth'], row['BirthDay']),
+                    'startDate': '2000,%s,%s,0,0,0' %(row['BirthMonth'], row['BirthDay']),
                     #'endDate':   '2000,%s,%s' %(row['BirthMonth'], str(int(row['BirthDay'])+1)),
                     #'type':      'default',
                     #'tag':       row['Tag'],
@@ -55,6 +56,8 @@ def csv2json(infile, outfile, xe=False):
                     dates[len(dates)-1]['asset']['thumbnail']='img_thumb/key_logo.png'
                 if row['BirthMonth']== '不明' or row['BirthMonth']=='' or row['BirthMonth']=='0':
                     dates[len(dates)-1]['startDate']='2001,1,1,0,0,1'
+                if row['Height']=='不明' or row['Weight']=='不明' or row['Height']=='' or row['Weight']=='':
+                        dates[len(dates)-1]['text'] = dates[len(dates)-1]['text'].replace('不明 cm','不明').replace('不明 kg','不明')
                 
                 ##~~~~~~
                 if xe:
@@ -67,6 +70,8 @@ def csv2json(infile, outfile, xe=False):
                         dates[len(dates)-1]['tag']='?杯'
                     else:
                         dates[len(dates)-1]['tag']=row['Cup']+'杯'
+    #sort by date, for show today's nearest birthday character
+    dates.sort(key= lambda x: datetime.datetime.strptime(x['startDate'], '%Y,%m,%d,%H,%M,%S'))
     
     data = {}
     if xe:
